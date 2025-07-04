987a78c032657cd93c5eb00e9f277b18abca2aa9
Carlos <carlos@appsfactory.com>
2025-07-04 13:38:51 +0200

    Crear endpoints para modificar datos

--- src/index.js
+++ src/index.js
@@ -20,28 +20,96 @@ async function readDatabase() {
     return JSON.parse(data);
   } catch (err) {
     console.error("Error reading database:", err);
-    return {};
+    return err;
+  }
+}
+
+async function writeDatabase(data) {
+  try {
+    await fs.writeFile(db, JSON.stringify(data, null, 2), 'utf8');
+    return true;
+  } catch (err) {
+    console.error("Error writing to database:", err);
+    return false;
   }
 }
 
 app.get("/api/equipos", (req, res) => {
   readDatabase()
-    .then(data => res.json(data.equipos || []))
-    .catch(err => res.status(500).send(`Error reading database ${err.message}`));
+    .then(data => res.json(data.equipos))
+    .catch(err => res.status(500).send(`Error reading database; ${err.message}`));
 });
 
 app.get("/api/mentores", (req, res) => {
   readDatabase()
-    .then(data => res.json(data.mentores || []))
-    .catch(err => res.status(500).send(`Error reading database ${err.message}`));
+    .then(data => res.json(data.mentores))
+    .catch(err => res.status(500).send(`Error reading database; ${err.message}`));
 });
 
-app.post("/api/equipos", (req, res) => {
-  res.send("Welcome to the API!");
+app.post("/api/equipos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id || isNaN(id) || id <= 0 || id > 4) {
+    return res.status(400).send("El ID del equipo debe ser un numero entre 1 y 4");
+  }
+
+  const mentores = req.body;
+  if (!Array.isArray(mentores) || mentores.length < 3 || mentores.length > 3) {
+    return res.status(400).send("Los mentores favoritos deben ser exactamente 3.");
+  }
+
+  mentores.forEach(mentor => {
+    if (isNaN(mentor) || mentor <= 0 || mentor > 4) {
+      return res.status(400).send("Los mentores deben ser numeros entre 0 y 4.");
+    }
+  });
+
+  if (new Set(mentores).size !== mentores.length) {
+    return res.status(400).send("Los mentores no deben repetirse.");
+  }
+
+  readDatabase().then(data => {
+    const index = data.equipos.findIndex(equipo => equipo.id === id);
+
+    data.equipos[index].mentores = mentores;
+
+    writeDatabase(data)
+      .then(() => res.status(201).json(data.equipos[index]))
+      .catch(err => res.status(500).send(`Error writing to database: ${err.message}`));
+
+  }).catch(err => res.status(500).send(`Error reading database: ${err.message}`));
 });
 
-app.post("/api/mentores", (req, res) => {
-  res.send("Welcome to the API!");
+app.post("/api/mentores/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id || isNaN(id) || id <= 0 || id > 4) {
+    return res.status(400).send("El ID del mentor debe ser un numero entre 1 y 4");
+  }
+
+  const equipos = req.body;
+  if (!Array.isArray(equipos) || equipos.length < 3 || equipos.length > 3) {
+    return res.status(400).send("Los mentores favoritos deben ser exactamente 3.");
+  }
+
+  equipos.forEach(equipo => {
+    if (isNaN(equipo) || equipo <= 0 || equipo > 4) {
+      return res.status(400).send("Los equipos deben ser numeros entre 0 y 4.");
+    }
+  });
+
+  if (new Set(equipos).size !== equipos.length) {
+    return res.status(400).send("Los equipos no deben repetirse.");
+  }
+
+  readDatabase().then(data => {
+    const index = data.mentores.findIndex(mentor => mentor.id === id);
+
+    data.mentores[index].equipos = equipos;
+
+    writeDatabase(data)
+      .then(() => res.status(201).json(data.mentores[index]))
+      .catch(err => res.status(500).send(`Error writing to database: ${err.message}`));
+
+  }).catch(err => res.status(500).send(`Error reading database: ${err.message}`));
 });
 
 app.listen(port, () => {
