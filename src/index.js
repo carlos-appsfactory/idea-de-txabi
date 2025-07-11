fd22bc6f3936942a22f187cbb4071fde05b2922b
Carlos <carlos@appsfactory.com>
2025-07-11 11:09:40 +0200

    Descomentar el matching

--- src/index.js
+++ src/index.js
@@ -168,13 +168,10 @@ app.get("/api/matching", async (req, res) => {
     // ruta donde guardaremos
     const resultadosFile = path.join(__dirname, "resultados.json");
 
-        res.json({});
+    // lo escribimos sobreescribiendo si existe
+    await fs.writeFile(resultadosFile, JSON.stringify(result, null, 2), "utf8");
 
-
-    // // // lo escribimos sobreescribiendo si existe
-    //  await fs.writeFile(resultadosFile, JSON.stringify(result, null, 2), "utf8");
-
-    // res.json(result);
+    res.json(result);
   } catch (err) {
     res.status(500).send(`Error ejecutando matching: ${err.message}`);
   }
